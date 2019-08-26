91a63fe64f9e57195d925e7014e7e2813d59f28c
Dim <cqd2202@yahoo.de>
2019-08-26 18:01:13 +0200

    DCSB-00020: Fxml Emf File #17

---
+++ de.dc.javafx.mm/src/de/dc/javafx/mm/file/FxmlFile.java
@@ -0,0 +1,27 @@
+package de.dc.javafx.mm.file;
+
+import org.eclipse.emf.ecore.EFactory;
+import org.eclipse.emf.ecore.EPackage;
+
+import de.dc.javafx.mm.EmfModel;
+import de.dc.javafx.mm.MmFactory;
+import de.dc.javafx.mm.MmPackage;
+
+public class FxmlFile extends EmfFile<EmfModel>{
+
+	@Override
+	public EPackage getEPackageEInstance() {
+		return MmPackage.eINSTANCE;
+	}
+
+	@Override
+	public EFactory getEFactoryEInstance() {
+		return MmFactory.eINSTANCE;
+	}
+
+	@Override
+	public String getExtension() {
+		return "javafx";
+	}
+
+}
