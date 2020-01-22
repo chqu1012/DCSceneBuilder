401e42a8213b2eefceabbabce90c0880205482d7
Dim <cqd2202@yahoo.de>
2020-01-22 21:17:10 +0100

    DCSB-00082: Fixed Controller Impl Template

--- de.dc.javafx.mm.editor/src/de/dc/javafx/mm/editor/template/ControllerImplTemplate.xtend
+++ de.dc.javafx.mm.editor/src/de/dc/javafx/mm/editor/template/ControllerImplTemplate.xtend
@@ -2,28 +2,58 @@ package de.dc.javafx.mm.editor.template
 
 import de.dc.javafx.mm.EBorderPane
 import de.dc.javafx.mm.ENode
+import de.dc.javafx.mm.ETableView
 import de.dc.javafx.mm.EmfModel
+import org.eclipse.emf.ecore.util.EcoreUtil
 
 class ControllerImplTemplate implements IGenerator<EmfModel> {
 
+	StringBuffer fieldBuffer = new StringBuffer
 	StringBuffer onActionBuffer = new StringBuffer
 
-	ControllerFieldInitializer onActionInitializer = new ControllerFieldInitializer(ControllerGenType.ON_ACTION_IMPL)
-
+	ControllerFieldInitializer fieldInitializer = new ControllerFieldInitializer(ControllerGenType.FIELD)
+	ControllerFieldInitializer onActionInitializer = new ControllerFieldInitializer(ControllerGenType.ON_ACTION)
+	
 	override gen(EmfModel t) '''
 	package «t.basePackage».controller;
-	
+		
+	import javafx.beans.value.*;
 	import javafx.event.*;
 	import org.apache.log4j.*;
+	import «t.basePackage».model.*;
 	
 	public class «t.name.toFirstUpper»Controller extends Base«t.name.toFirstUpper»Controller{
 		
-		private Logger log = Logger.getLogger(PersonController.class);
+		private Logger log = Logger.getLogger(«t.name.toFirstUpper»Controller.class);
+		
+		private «t.name»Binding model = new «t.name»Binding();
 		
 		@Override
 		public void initialize() {
+			super.initialize();
 			log.info("Initialize  «t.name.toFirstUpper»Controller");
+			
+			«FOR node : EcoreUtil.getAllContents(t, true).filter(ETableView).toList»
+			«val modelName = node.model?.name.toFirstUpper»
+			«IF !modelName.isNullOrEmpty»
+			model.sortedData«modelName»().comparatorProperty().bind(tableView«modelName».comparatorProperty());			
+			«node.id.toFirstLower».setItems(model.sortedData«node.model.name.toFirstUpper»());
+			«node.id.toFirstLower».getSelectionModel().selectedItemProperty().addListener(this::onTableView«node.model.name.toFirstUpper»SelectionChanged);
+			model.selected«node.model.name.toFirstUpper».bind(tableView«node.model.name.toFirstUpper».getSelectionModel().selectedItemProperty());
+			«ENDIF»
+			«ENDFOR»
 		}
+		
+		«FOR node : EcoreUtil.getAllContents(t, true).filter(ETableView).toList»
+		«val name = node.model?.name.toFirstUpper»
+		«IF !name.isNullOrEmpty»
+		private void onTableView«name»SelectionChanged(ObservableValue<? extends «name»> observable, «name» oldValue, «name» newValue) {
+			if (newValue!=null) {
+				// TODO: not impleted yet!
+			}
+		}
+		«ENDIF»
+		«ENDFOR»
 		
 		«t.root.initField»
 		«onActionBuffer.toString»
@@ -32,7 +62,10 @@ class ControllerImplTemplate implements IGenerator<EmfModel> {
 
 	def void getInitField(ENode node) {
 		if (node !== null) {
-			onActionBuffer.append = '''«onActionInitializer.doSwitch(node)»'''
+			if (!node.id.isNullOrEmpty) {
+				fieldBuffer.append = '''«fieldInitializer.doSwitch(node)»'''
+				onActionBuffer.append = '''«onActionInitializer.doSwitch(node)»'''
+			}
 			if (node instanceof EBorderPane) {
 				node.left.initField
 				node.right.initField
--- de.dc.javafx.mm.editor/xtend-gen/de/dc/javafx/mm/editor/template/ControllerImplTemplate.java
+++ de.dc.javafx.mm.editor/xtend-gen/de/dc/javafx/mm/editor/template/ControllerImplTemplate.java
@@ -1,20 +1,30 @@
 package de.dc.javafx.mm.editor.template;
 
+import com.google.common.collect.Iterators;
 import de.dc.javafx.mm.EBorderPane;
 import de.dc.javafx.mm.ENode;
+import de.dc.javafx.mm.ETableView;
+import de.dc.javafx.mm.ETableViewModel;
 import de.dc.javafx.mm.EmfModel;
 import de.dc.javafx.mm.editor.template.ControllerFieldInitializer;
 import de.dc.javafx.mm.editor.template.ControllerGenType;
 import de.dc.javafx.mm.editor.template.IGenerator;
+import java.util.List;
 import java.util.function.Consumer;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.IteratorExtensions;
 import org.eclipse.xtext.xbase.lib.StringExtensions;
 
 @SuppressWarnings("all")
 public class ControllerImplTemplate implements IGenerator<EmfModel> {
+  private StringBuffer fieldBuffer = new StringBuffer();
+  
   private StringBuffer onActionBuffer = new StringBuffer();
   
-  private ControllerFieldInitializer onActionInitializer = new ControllerFieldInitializer(ControllerGenType.ON_ACTION_IMPL);
+  private ControllerFieldInitializer fieldInitializer = new ControllerFieldInitializer(ControllerGenType.FIELD);
+  
+  private ControllerFieldInitializer onActionInitializer = new ControllerFieldInitializer(ControllerGenType.ON_ACTION);
   
   @Override
   public String gen(final EmfModel t) {
@@ -24,11 +34,19 @@ public class ControllerImplTemplate implements IGenerator<EmfModel> {
     _builder.append(_basePackage);
     _builder.append(".controller;");
     _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("import javafx.beans.value.*;");
     _builder.newLine();
     _builder.append("import javafx.event.*;");
     _builder.newLine();
     _builder.append("import org.apache.log4j.*;");
     _builder.newLine();
+    _builder.append("import ");
+    String _basePackage_1 = t.getBasePackage();
+    _builder.append(_basePackage_1);
+    _builder.append(".model.*;");
+    _builder.newLineIfNotEmpty();
     _builder.newLine();
     _builder.append("public class ");
     String _firstUpper = StringExtensions.toFirstUpper(t.getName());
@@ -41,8 +59,22 @@ public class ControllerImplTemplate implements IGenerator<EmfModel> {
     _builder.append("\t");
     _builder.newLine();
     _builder.append("\t");
-    _builder.append("private Logger log = Logger.getLogger(PersonController.class);");
+    _builder.append("private Logger log = Logger.getLogger(");
+    String _firstUpper_2 = StringExtensions.toFirstUpper(t.getName());
+    _builder.append(_firstUpper_2, "\t");
+    _builder.append("Controller.class);");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
     _builder.newLine();
+    _builder.append("\t");
+    _builder.append("private ");
+    String _name = t.getName();
+    _builder.append(_name, "\t");
+    _builder.append("Binding model = new ");
+    String _name_1 = t.getName();
+    _builder.append(_name_1, "\t");
+    _builder.append("Binding();");
+    _builder.newLineIfNotEmpty();
     _builder.append("\t");
     _builder.newLine();
     _builder.append("\t");
@@ -52,14 +84,117 @@ public class ControllerImplTemplate implements IGenerator<EmfModel> {
     _builder.append("public void initialize() {");
     _builder.newLine();
     _builder.append("\t\t");
+    _builder.append("super.initialize();");
+    _builder.newLine();
+    _builder.append("\t\t");
     _builder.append("log.info(\"Initialize  ");
-    String _firstUpper_2 = StringExtensions.toFirstUpper(t.getName());
-    _builder.append(_firstUpper_2, "\t\t");
+    String _firstUpper_3 = StringExtensions.toFirstUpper(t.getName());
+    _builder.append(_firstUpper_3, "\t\t");
     _builder.append("Controller\");");
     _builder.newLineIfNotEmpty();
+    _builder.append("\t\t");
+    _builder.newLine();
+    {
+      List<ETableView> _list = IteratorExtensions.<ETableView>toList(Iterators.<ETableView>filter(EcoreUtil.<Object>getAllContents(t, true), ETableView.class));
+      for(final ETableView node : _list) {
+        _builder.append("\t\t");
+        ETableViewModel _model = node.getModel();
+        String _name_2 = null;
+        if (_model!=null) {
+          _name_2=_model.getName();
+        }
+        final String modelName = StringExtensions.toFirstUpper(_name_2);
+        _builder.newLineIfNotEmpty();
+        {
+          boolean _isNullOrEmpty = StringExtensions.isNullOrEmpty(modelName);
+          boolean _not = (!_isNullOrEmpty);
+          if (_not) {
+            _builder.append("\t\t");
+            _builder.append("model.sortedData");
+            _builder.append(modelName, "\t\t");
+            _builder.append("().comparatorProperty().bind(tableView");
+            _builder.append(modelName, "\t\t");
+            _builder.append(".comparatorProperty());\t\t\t");
+            _builder.newLineIfNotEmpty();
+            _builder.append("\t\t");
+            String _firstLower = StringExtensions.toFirstLower(node.getId());
+            _builder.append(_firstLower, "\t\t");
+            _builder.append(".setItems(model.sortedData");
+            String _firstUpper_4 = StringExtensions.toFirstUpper(node.getModel().getName());
+            _builder.append(_firstUpper_4, "\t\t");
+            _builder.append("());");
+            _builder.newLineIfNotEmpty();
+            _builder.append("\t\t");
+            String _firstLower_1 = StringExtensions.toFirstLower(node.getId());
+            _builder.append(_firstLower_1, "\t\t");
+            _builder.append(".getSelectionModel().selectedItemProperty().addListener(this::onTableView");
+            String _firstUpper_5 = StringExtensions.toFirstUpper(node.getModel().getName());
+            _builder.append(_firstUpper_5, "\t\t");
+            _builder.append("SelectionChanged);");
+            _builder.newLineIfNotEmpty();
+            _builder.append("\t\t");
+            _builder.append("model.selected");
+            String _firstUpper_6 = StringExtensions.toFirstUpper(node.getModel().getName());
+            _builder.append(_firstUpper_6, "\t\t");
+            _builder.append(".bind(tableView");
+            String _firstUpper_7 = StringExtensions.toFirstUpper(node.getModel().getName());
+            _builder.append(_firstUpper_7, "\t\t");
+            _builder.append(".getSelectionModel().selectedItemProperty());");
+            _builder.newLineIfNotEmpty();
+          }
+        }
+      }
+    }
     _builder.append("\t");
     _builder.append("}");
     _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    {
+      List<ETableView> _list_1 = IteratorExtensions.<ETableView>toList(Iterators.<ETableView>filter(EcoreUtil.<Object>getAllContents(t, true), ETableView.class));
+      for(final ETableView node_1 : _list_1) {
+        _builder.append("\t");
+        ETableViewModel _model_1 = node_1.getModel();
+        String _name_3 = null;
+        if (_model_1!=null) {
+          _name_3=_model_1.getName();
+        }
+        final String name = StringExtensions.toFirstUpper(_name_3);
+        _builder.newLineIfNotEmpty();
+        {
+          boolean _isNullOrEmpty_1 = StringExtensions.isNullOrEmpty(name);
+          boolean _not_1 = (!_isNullOrEmpty_1);
+          if (_not_1) {
+            _builder.append("\t");
+            _builder.append("private void onTableView");
+            _builder.append(name, "\t");
+            _builder.append("SelectionChanged(ObservableValue<? extends ");
+            _builder.append(name, "\t");
+            _builder.append("> observable, ");
+            _builder.append(name, "\t");
+            _builder.append(" oldValue, ");
+            _builder.append(name, "\t");
+            _builder.append(" newValue) {");
+            _builder.newLineIfNotEmpty();
+            _builder.append("\t");
+            _builder.append("\t");
+            _builder.append("if (newValue!=null) {");
+            _builder.newLine();
+            _builder.append("\t");
+            _builder.append("\t\t");
+            _builder.append("// TODO: not impleted yet!");
+            _builder.newLine();
+            _builder.append("\t");
+            _builder.append("\t");
+            _builder.append("}");
+            _builder.newLine();
+            _builder.append("\t");
+            _builder.append("}");
+            _builder.newLine();
+          }
+        }
+      }
+    }
     _builder.append("\t");
     _builder.newLine();
     _builder.append("\t");
@@ -76,10 +211,18 @@ public class ControllerImplTemplate implements IGenerator<EmfModel> {
   
   public void getInitField(final ENode node) {
     if ((node != null)) {
-      StringConcatenation _builder = new StringConcatenation();
-      String _doSwitch = this.onActionInitializer.doSwitch(node);
-      _builder.append(_doSwitch);
-      this.onActionBuffer.append(_builder);
+      boolean _isNullOrEmpty = StringExtensions.isNullOrEmpty(node.getId());
+      boolean _not = (!_isNullOrEmpty);
+      if (_not) {
+        StringConcatenation _builder = new StringConcatenation();
+        String _doSwitch = this.fieldInitializer.doSwitch(node);
+        _builder.append(_doSwitch);
+        this.fieldBuffer.append(_builder);
+        StringConcatenation _builder_1 = new StringConcatenation();
+        String _doSwitch_1 = this.onActionInitializer.doSwitch(node);
+        _builder_1.append(_doSwitch_1);
+        this.onActionBuffer.append(_builder_1);
+      }
       if ((node instanceof EBorderPane)) {
         this.getInitField(((EBorderPane)node).getLeft());
         this.getInitField(((EBorderPane)node).getRight());
