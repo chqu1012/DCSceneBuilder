78d923ae91a3e2f0e83e2e7eac84aca987decade
Dim <cqd2202@yahoo.de>
2020-01-29 22:34:40 +0100

    DCSB-00113: Enhanced the FxmlRenderer with further controls

--- de.dc.javafx.mm.renderer/src/de/dc/javafx/mm/renderer/FxmlRenderer.java
+++ de.dc.javafx.mm.renderer/src/de/dc/javafx/mm/renderer/FxmlRenderer.java
@@ -13,35 +13,65 @@ import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
+import de.dc.javafx.mm.EAnchorPane;
 import de.dc.javafx.mm.EBorderPane;
 import de.dc.javafx.mm.EButton;
+import de.dc.javafx.mm.ECheckBox;
 import de.dc.javafx.mm.EFilteredTableView;
+import de.dc.javafx.mm.EFlowPane;
+import de.dc.javafx.mm.EGridPane;
 import de.dc.javafx.mm.EHBox;
+import de.dc.javafx.mm.EHyperlink;
 import de.dc.javafx.mm.EInsets;
 import de.dc.javafx.mm.ELabel;
 import de.dc.javafx.mm.EListView;
 import de.dc.javafx.mm.ENode;
+import de.dc.javafx.mm.EPane;
+import de.dc.javafx.mm.ERadioButton;
+import de.dc.javafx.mm.EScrollPane;
+import de.dc.javafx.mm.ESplitPane;
+import de.dc.javafx.mm.EStackPane;
+import de.dc.javafx.mm.ETabPane;
 import de.dc.javafx.mm.ETableColumn;
 import de.dc.javafx.mm.ETableView;
 import de.dc.javafx.mm.EText;
+import de.dc.javafx.mm.ETilePane;
+import de.dc.javafx.mm.EToogleButton;
 import de.dc.javafx.mm.EVBox;
+import de.dc.javafx.mm.EWebView;
 import de.dc.javafx.mm.EmfModel;
 import de.dc.javafx.mm.control.FilteredTableView;
 import de.dc.javafx.mm.util.MmSwitch;
 import javafx.event.Event;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
 import javafx.util.Callback;
 
 public class FxmlRenderer extends MmSwitch<Node> {
@@ -177,6 +207,161 @@ public class FxmlRenderer extends MmSwitch<Node> {
 	}
 
 	@Override
+	public Node caseEScrollPane(EScrollPane object) {
+		ScrollPane node = new ScrollPane();
+		node.setContent(doSwitch(object.getContent()));
+		node.setFitToHeight(object.isFitToHeight());
+		node.setFitToWidth(object.isFitToWidth());
+		node.setPannable(object.isIsPannable());
+		initSize(object, node);
+		return node;
+	}
+
+	@Override
+	public Node caseETabPane(ETabPane object) {
+		TabPane node = new TabPane();
+		object.getTabs().forEach(e->node.getTabs().add(new Tab(e.getName(), doSwitch(e.getContent()))));
+		initSize(object, node);
+		return node;
+	}
+
+@Override
+	public Node caseECheckBox(ECheckBox object) {
+		CheckBox node = new CheckBox(object.getText());
+		initSize(object, node);
+		return node;
+	}
+	
+	@Override
+	public Node caseEHyperlink(EHyperlink object) {
+		Hyperlink node = new Hyperlink(object.getText());
+		initSize(object, node);
+		return node;
+	}
+	
+	@Override
+	public Node caseERadioButton(ERadioButton object) {
+		RadioButton node = new RadioButton(object.getText());
+		initSize(object, node);
+		return node;
+	}
+	
+	@Override
+	public Node caseEToogleButton(EToogleButton object) {
+		ToggleButton node = new ToggleButton(object.getText());
+		initSize(object, node);
+		return node;
+	}
+	
+	@Override
+	public Node caseEWebView(EWebView object) {
+		WebView node = new WebView();
+		node.getEngine().load(object.getUrl());
+		node.setContextMenuEnabled(object.isContextMenuEnabled());
+		initId(object, node);
+		initMargin(object, node);
+		return node;
+	}
+
+	@Override
+	public Node caseETilePane(ETilePane object) {
+		TitledPane node = new TitledPane();
+		initSize(object, node);
+		return node;
+	}
+
+	@Override
+	public Node caseEPane(EPane object) {
+		Pane node = new Pane();
+		initSize(object, node);
+		return node;
+	}
+
+	@Override
+	public Node caseEFlowPane(EFlowPane object) {
+		FlowPane node = new FlowPane();
+		initSize(object, node);
+		return node;
+	}
+
+	@Override
+	public Node caseEGridPane(EGridPane object) {
+		GridPane node = new GridPane();
+		initSize(object, node);
+		return node;
+	}
+
+@Override
+	public Node caseEAnchorPane(EAnchorPane object) {
+		AnchorPane node = new AnchorPane();
+		initSize(object, node);
+		return node;
+	}
+	
+	@Override
+	public Node caseESplitPane(ESplitPane object) {
+		SplitPane node = new SplitPane();
+		initSize(object, node);
+		return node;
+	}
+	
+	@Override
+	public Node caseEStackPane(EStackPane object) {
+		StackPane node = new StackPane();
+		initSize(object, node);
+		return node;
+	}
+
+
+private void initId(ENode object, Node node) {
+		if (object.getId()!=null) {
+			controlRegistry.put(object.getId(), node);
+		}
+	}
+	
+	private void initMargin(ENode object, Parent node) {
+		EInsets margin = object.getMargin();
+		if (margin!=null) {
+			Insets marginInsets = new Insets(margin.getTop(), margin.getRight(), margin.getBottom(), margin.getLeft());
+			if (node instanceof VBox) {
+				VBox.setMargin(node, marginInsets);
+			}else if (node instanceof GridPane) {
+				GridPane.setMargin(node, marginInsets);
+			}else if (node instanceof HBox) {
+				HBox.setMargin(node, marginInsets);
+			}else if (node instanceof BorderPane) {
+				BorderPane.setMargin(node, marginInsets);
+			}else if (node instanceof FlowPane) {
+				FlowPane.setMargin(node, marginInsets);
+			}else if (node instanceof StackPane) {
+				StackPane.setMargin(node, marginInsets);
+			}else if (node instanceof TilePane) {
+				TilePane.setMargin(node, marginInsets);
+			}
+		}
+	}
+
+	protected void initSize(ENode object, Region node) {
+		node.setMinHeight(object.getMinHeight() == 0 ? Double.MIN_VALUE : object.getMinHeight());
+		node.setMinWidth(object.getMinWidth() == 0 ? Double.MIN_VALUE : object.getMinWidth());
+		node.setMaxHeight(object.getMaxHeight() == 0 ? Double.MAX_VALUE : object.getMaxHeight());
+		node.setMaxWidth(object.getMaxWidth() == 0 ? Double.MAX_VALUE : object.getMaxWidth());
+		if (object.getPrefHeight() > 0) {
+			node.setPrefHeight(object.getPrefHeight());
+		}
+		if (object.getPrefWidth() > 0) {
+			node.setPrefWidth(object.getPrefWidth());
+		}
+		if (object.getPadding()!=null) {
+			EInsets padding = object.getPadding();
+			node.setPadding(new Insets(padding.getTop(), padding.getRight(), padding.getBottom(), padding.getLeft()));
+		}
+		if (node instanceof Pane) {
+			initMargin(object, (Pane) node);
+		}
+	}
+	
+	@Override
 	public Node caseEText(EText object) {
 		TextField node = new TextField();
 		node.setPromptText(object.getPromtText());
@@ -262,18 +447,5 @@ public class FxmlRenderer extends MmSwitch<Node> {
 
 		object.getChildren().forEach(e -> addChild(node, e));
 		return node;
-	}
-
-	protected void initSize(ENode object, Region node) {
-		node.setMinHeight(object.getMinHeight() == 0 ? Double.MIN_VALUE : object.getMinHeight());
-		node.setMinWidth(object.getMinWidth() == 0 ? Double.MIN_VALUE : object.getMinWidth());
-		node.setMaxHeight(object.getMaxHeight() == 0 ? Double.MAX_VALUE : object.getMaxHeight());
-		node.setMaxWidth(object.getMaxWidth() == 0 ? Double.MAX_VALUE : object.getMaxWidth());
-		if (object.getPrefHeight() > 0) {
-			node.setPrefHeight(object.getPrefHeight());
-		}
-		if (object.getPrefWidth() > 0) {
-			node.setPrefWidth(object.getPrefWidth());
-		}
 	}
 }
