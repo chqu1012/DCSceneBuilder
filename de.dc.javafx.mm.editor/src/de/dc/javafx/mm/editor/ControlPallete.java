71efbf8d48bf332d9f11427c95cb040b3e51e766
Dim <cqd2202@yahoo.de>
2020-02-02 18:20:19 +0100

    DCSB-00119: Enhanced the ControlPallet with Generation section

--- de.dc.javafx.mm.editor/src/de/dc/javafx/mm/editor/ControlPallete.java
+++ de.dc.javafx.mm.editor/src/de/dc/javafx/mm/editor/ControlPallete.java
@@ -1,5 +1,11 @@
 package de.dc.javafx.mm.editor;
 
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.NotEnabledException;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -15,6 +21,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -22,6 +29,8 @@ import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.part.ViewPart;
 
 import de.dc.javafx.mm.EBinding;
@@ -162,6 +171,64 @@ public class ControlPallete extends ViewPart {
 		});
 		btnTextproperty.setText("TextProperty");
 		
+		TabItem tbtmGeneration = new TabItem(tabFolder, SWT.NONE);
+		tbtmGeneration.setText("Generation");
+		
+		Group groupGeneration = new Group(tabFolder, SWT.NONE);
+		tbtmGeneration.setControl(groupGeneration);
+		groupGeneration.setLayout(new GridLayout(2, false));
+		
+		Button btnGenerateModel = new Button(groupGeneration, SWT.NONE);
+		btnGenerateModel.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				executeCommand("de.dc.javafx.mm.editor.command.generateModelClass");
+			}
+		});
+		btnGenerateModel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		btnGenerateModel.setText("Generate Model");
+		
+		Button btnGenerateBinding = new Button(groupGeneration, SWT.NONE);
+		btnGenerateBinding.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				executeCommand("de.dc.javafx.mm.editor.command.generateBindingClass");
+			}
+		});
+		btnGenerateBinding.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		btnGenerateBinding.setText("Generate Binding");
+		
+		Button btnGenerateController = new Button(groupGeneration, SWT.NONE);
+		btnGenerateController.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				executeCommand("de.dc.javafx.mm.editor.command.generateControllerClass");
+			}
+		});
+		btnGenerateController.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		btnGenerateController.setBounds(0, 0, 75, 25);
+		btnGenerateController.setText("Generate Base Controller");
+		
+		Button btnGenerateApplication = new Button(groupGeneration, SWT.NONE);
+		btnGenerateApplication.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				executeCommand("de.dc.javafx.mm.editor.command.generateApplication");
+			}
+		});
+		btnGenerateApplication.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		btnGenerateApplication.setText("Generate Application");
+		
+		Button btnGenerateExtendedcontrollerr = new Button(groupGeneration, SWT.NONE);
+		btnGenerateExtendedcontrollerr.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				executeCommand("de.dc.javafx.mm.editor.command.generateControllerImplClass");
+			}
+		});
+		btnGenerateExtendedcontrollerr.setBounds(0, 0, 75, 25);
+		btnGenerateExtendedcontrollerr.setText("Generate Extended Controller");
+		
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayout(new GridLayout(2, false));
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
@@ -177,6 +244,25 @@ public class ControlPallete extends ViewPart {
 		compositeForm.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
 	}
 
+	private void executeCommand(String commandId) {
+		// Retrieve the corresponding Services
+		IHandlerService handlerService = (IHandlerService) getSite().getService(IHandlerService.class);
+		ICommandService commandService = (ICommandService) getSite().getService(ICommandService.class);
+		
+		// Retrieve the command
+		Command generateCmd = commandService.getCommand(commandId);
+
+		// Create an ExecutionEvent
+		ExecutionEvent executionEvent = handlerService.createExecutionEvent(generateCmd, new Event());
+
+		// Launch the command
+		try {
+			generateCmd.executeWithChecks(executionEvent);
+		} catch (ExecutionException | NotDefinedException | NotEnabledException | NotHandledException e1) {
+			e1.printStackTrace();
+		}		
+	}
+	
 	@Override
 	public void setFocus() {
 	}
